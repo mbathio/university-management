20c8259e282322842d642b78c37b901dc6243e9a
mbathio <mbathiobadiallo20@gmail.com>
2025-04-21 12:18:01 +0200

    Initial commit: university-management

--- src/main/java/com/uchk/university/security/JwtTokenUtil.java
+++ src/main/java/com/uchk/university/security/JwtTokenUtil.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
